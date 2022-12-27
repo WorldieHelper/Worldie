d0101d8dd0eaa861acc405bb0888e0759a8fa25f
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2022-12-26 20:39:01 -0500

    More globals + Update 0.0.1

--- source/main.lua
+++ source/main.lua
@@ -1,20 +1,37 @@
+local Version = "0.0.1"
+
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
-local Name = LocalPlayer.Name
 
---> Wait for Game to load
 repeat
     task.wait()
 until game:IsLoaded()
 
 rconsoleclear()
-rconsolename("Worldie")
+rconsolename("Worldie v" .. Version)
 
 local Shared = ReplicatedStorage:WaitForChild("Shared")
 local EventSpinner = Shared:FindFirstChild("EventSpinner")
 
-local LocalProfile = ReplicatedStorage:WaitForChild("Profiles"):WaitForChild(Name)
+local LocalProfile = ReplicatedStorage:WaitForChild("Profiles"):WaitForChild(LocalPlayer.Name)
 local Inventory = LocalProfile:FindFirstChild("Inventory")
 local Items = Inventory:FindFirstChild("Items")
 local Cosmetics = Inventory:FindFirstChild("Cosmetics")
+
+local function printl(...)
+    rconsoleprint(... .. "\n")
+end
+
+local function printe(...)
+    rconsoleerr(... .. "\n")
+end
+
+local function printw(...)
+    rconsolewarn(... .. "\n")
+end
+
+local function printi(...)
+    rconsoleinfo(... .. "\n")
+end
+
